e41a3ef31ce5220b96cb5fd3733b5e4b0a02a04e
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 18:32:53 +0300

    Local State ile CheckBox Oluşturma

--- lib/pages/gorevler_tile.dart
+++ lib/pages/gorevler_tile.dart
@@ -8,7 +8,28 @@ class GorevTile extends StatelessWidget {
         "Görev 1",
         style: TextStyle(color: Color.fromRGBO(245, 224, 211, 1)),
       ),
-      trailing: Checkbox(value: false, onChanged: (bool val) {}),
+      trailing: GorevCheckBox(),
+    );
+  }
+}
+
+class GorevCheckBox extends StatefulWidget {
+  @override
+  _GorevCheckBoxState createState() => _GorevCheckBoxState();
+}
+
+class _GorevCheckBoxState extends State<GorevCheckBox> {
+  bool secim = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: secim,
+      onChanged: (bool val) {
+        setState(() {
+          secim = val;
+        });
+      },
+      activeColor: Color.fromRGBO(77, 59, 59, 1),
     );
   }
 }
