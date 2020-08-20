a61d01c41c53384f6cd8f58de9e36226635c257b
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 19:07:42 +0300

    Görev Listesi Oluşturma

--- lib/pages/gorevler_list.dart
+++ lib/pages/gorevler_list.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:training38_gorev_listesi_app/pages/gorevler_tile.dart';
+import 'package:training38_gorev_listesi_app/models/gorev.dart';
 
-class GorevListesi extends StatelessWidget {
+class GorevListesi extends StatefulWidget {
+  @override
+  _GorevListesiState createState() => _GorevListesiState();
+}
+
+class _GorevListesiState extends State<GorevListesi> {
+  List<Gorev> gorevListesi = [
+    Gorev(gorevAd: "Spora Git"),
+    Gorev(gorevAd: "Effortless English çalış"),
+    Gorev(gorevAd: "Mehmet Yılmaz'ı öp"),
+    Gorev(gorevAd: "Flutter Çalış"),
+    Gorev(gorevAd: "Mehmet Yılmaz'a el salla"),
+  ];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        GorevTile(),
-        GorevTile(),
-        GorevTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return GorevTile(
+          gorevAd: gorevListesi[index].gorevAd,
+          secim: gorevListesi[index].yapildi,
+          checkBoxCallback: (bool val) {
+            setState(() {
+              gorevListesi[index].toggleYapildi();
+            });
+          },
+        );
+      },
+      itemCount: gorevListesi.length,
     );
   }
 }
--- lib/pages/gorevler_tile.dart
+++ lib/pages/gorevler_tile.dart
@@ -1,52 +1,26 @@
 import 'package:flutter/material.dart';
 
-class GorevTile extends StatefulWidget {
-  @override
-  _GorevTileState createState() => _GorevTileState();
-}
+class GorevTile extends StatelessWidget {
+  final bool secim;
+  final String gorevAd;
+  final Function checkBoxCallback;
 
-class _GorevTileState extends State<GorevTile> {
-  // Global State ile checkBox oluşturmada: GorevCheckBox daki özellikler değiştiriliyo
-  // fakat bu _GorevTileState değiştirilerek bu işlem yapılıyo.
-  bool secim = false;
-  void secimDegistirmeFonk(bool yeniSecim) {
-    setState(() {
-      secim = yeniSecim;
-    });
-  }
+  GorevTile({this.secim, this.gorevAd, this.checkBoxCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        "Görev 1",
+        gorevAd,
         style: TextStyle(
-            decoration: secim ? TextDecoration.lineThrough : null,
-            color: Color.fromRGBO(245, 224, 211, 1)),
+            color: Color.fromRGBO(245, 224, 211, 1),
+            decoration: secim ? TextDecoration.lineThrough : null),
       ),
-      trailing: GorevCheckBox(
-        secim: secim,
-        secimDegistir: secimDegistirmeFonk,
+      trailing: Checkbox(
+        value: secim,
+        onChanged: checkBoxCallback,
+        activeColor: Color.fromRGBO(77, 59, 59, 1),
       ),
-    );
-  }
-}
-
-class GorevCheckBox extends StatelessWidget {
-  final bool secim;
-  final Function secimDegistir;
-
-  GorevCheckBox({this.secim, this.secimDegistir});
-
-  // Local State: bir state içerisinde değişken oluşturup,
-  // yine o state içerisinde o değişken kullanılıyorsa bu Local State'dir.
-  // secim isimli bir değişken oluşturuldu, setState içerisinde de kullanıldı.
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: secim,
-      onChanged: secimDegistir,
-      activeColor: Color.fromRGBO(77, 59, 59, 1),
     );
   }
 }
