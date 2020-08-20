19e0c58425e59d63d0c324f3c5aa23b5d2c54bb4
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 22:50:57 +0300

    Consumer Kullanımı

--- lib/pages/gorevler_list.dart
+++ lib/pages/gorevler_list.dart
@@ -5,19 +5,25 @@ import 'package:provider/provider.dart';
 
 class GorevListesi extends StatelessWidget {
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return GorevTile(
-          gorevAd: Provider.of<GorevVeri>(context).gorevListesi[index].gorevAd,
-          secim: Provider.of<GorevVeri>(context).gorevListesi[index].yapildi,
-          checkBoxCallback: (bool val) {
-            // setState(() {
-            //   Provider.of<GorevVeri>(context).gorevListesi[index].toggleYapildi();
-            // });
+    return Consumer<GorevVeri>(
+      builder: (context, gorevVeri, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return GorevTile(
+              gorevAd:
+                  Provider.of<GorevVeri>(context).gorevListesi[index].gorevAd,
+              secim:
+                  Provider.of<GorevVeri>(context).gorevListesi[index].yapildi,
+              checkBoxCallback: (bool val) {
+                // setState(() {
+                //   Provider.of<GorevVeri>(context).gorevListesi[index].toggleYapildi();
+                // });
+              },
+            );
           },
+          itemCount: Provider.of<GorevVeri>(context).gorevSayisi,
         );
       },
-      itemCount: Provider.of<GorevVeri>(context).gorevListesi.length,
     );
   }
 }
