97068fac00a561d6a0d81e4d9e856cf4a2641964
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 18:44:30 +0300

    Global State ile CheckBox Oluşturma

--- lib/pages/gorevler_tile.dart
+++ lib/pages/gorevler_tile.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 
-class GorevTile extends StatelessWidget {
+class GorevTile extends StatefulWidget {
+  @override
+  _GorevTileState createState() => _GorevTileState();
+}
+
+class _GorevTileState extends State<GorevTile> {
+  // Global State ile checkBox oluşturmada: GorevCheckBox daki özellikler değiştiriliyo
+  // fakat bu _GorevTileState değiştirilerek bu işlem yapılıyo.
+  bool secim = false;
+  void secimDegistirmeFonk(bool yeniSecim) {
+    setState(() {
+      secim = yeniSecim;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         "Görev 1",
-        style: TextStyle(color: Color.fromRGBO(245, 224, 211, 1)),
+        style: TextStyle(
+            decoration: secim ? TextDecoration.lineThrough : null,
+            color: Color.fromRGBO(245, 224, 211, 1)),
       ),
-      trailing: GorevCheckBox(),
+      trailing: GorevCheckBox(
+        secim: secim,
+        secimDegistir: secimDegistirmeFonk,
+      ),
     );
   }
 }
 
-class GorevCheckBox extends StatefulWidget {
-  @override
-  _GorevCheckBoxState createState() => _GorevCheckBoxState();
-}
+class GorevCheckBox extends StatelessWidget {
+  final bool secim;
+  final Function secimDegistir;
 
-class _GorevCheckBoxState extends State<GorevCheckBox> {
-  bool secim = false;
+  GorevCheckBox({this.secim, this.secimDegistir});
+
+  // Local State: bir state içerisinde değişken oluşturup,
+  // yine o state içerisinde o değişken kullanılıyorsa bu Local State'dir.
+  // secim isimli bir değişken oluşturuldu, setState içerisinde de kullanıldı.
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       value: secim,
-      onChanged: (bool val) {
-        setState(() {
-          secim = val;
-        });
-      },
+      onChanged: secimDegistir,
       activeColor: Color.fromRGBO(77, 59, 59, 1),
     );
   }
