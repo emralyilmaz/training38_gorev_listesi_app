3b81d7713e88b6b43f498960f95996d136a29475
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 14:28:38 +0300

    Görevler Ekranını Tasarlama

---
+++ android/app/src/main/kotlin/com/example/training38_gorev_listesi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.training38_gorev_listesi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/gorevler_ekran.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class GorevlerEkran extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(245, 224, 211, 1),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Color.fromRGBO(222, 98, 98, 1),
+        ),
+        backgroundColor: Color.fromRGBO(77, 59, 59, 1),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, bottom: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 40,
+                    color: Color.fromRGBO(245, 224, 211, 1),
+                  ),
+                  backgroundColor: Color.fromRGBO(77, 59, 59, 1),
+                  radius: 30,
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  "Görev Yöneticisi",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  "5 tane görev var",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(222, 98, 98, 1),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
